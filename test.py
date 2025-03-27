d551775ec326a334c08529ddf18b9f2f089e0c74
CuiWJ <150432263+cuiwjTech@users.noreply.github.com>
2025-03-27 22:58:18 +0800

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import torch_geometric
-from torch_geometric.datasets import Planetoid, Amazon, Coauthor,Airports,GitHub
-data_cora = Planetoid(root='./data',name='Pubmed')
-data_amazon = Amazon(root='./data',name='Photo')
-print(data_amazon)
